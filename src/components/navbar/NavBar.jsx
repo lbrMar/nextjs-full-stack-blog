38ae12259f0cae6515af1e1d2d7cbd0399a9ad00
lbrMar <mar.lm@protonmail.com>
2024-02-16 19:11:37 -0800

    components layout, error page, loading page, not found page

---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,14 @@
+import Links from "@/components/navbar/links/Links"
+
+export default function NavBar() {
+  return (
+    <div>
+      <div>
+        Logo
+      </div>
+      <div>        
+        <Links />
+      </div>
+    </div>
+  )
+}

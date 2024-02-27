5b9ee1b756423538f5e83fcf11e44fc4c26d3886
lbrMar <mar.lm@protonmail.com>
2024-02-26 17:56:35 -0800

    edit linkmenu

--- src/components/navbar/linkmenu/LinkMenu.jsx
+++ src/components/navbar/linkmenu/LinkMenu.jsx
@@ -1,50 +1,30 @@
 "use client"
 
 import styles from './linkmenu.module.css'
-import { HamburgerMenuIcon } from '@radix-ui/react-icons'
+import { useState } from "react"
+import { HamburgerMenuIcon } from "@radix-ui/react-icons"
 import NavLink from '@/components/navbar/links/navlink/NavLink'
-import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 
 export default function LinkMenu({ linkItems }) {
+  const [toggleMenu, setToggelMenu] = useState('closed')
+
+  const onMenuClick = () => {
+    setToggelMenu((prev) => prev === 'closed' ? 'open' : 'closed')
+  }
   return (
     <div>
-      <DropdownMenu.Root>
-        <DropdownMenu.Trigger asChild>
-          <button className={`${styles.IconButton}`} aria-label="Customise options">
-            <HamburgerMenuIcon className={`${styles.hamburgerIcon}`}/>
-          </button>
-        </DropdownMenu.Trigger>
-
-        <DropdownMenu.Portal>
-          <DropdownMenu.Content className={`${styles.menuContainer}`}>
-            {/* <DropdownMenu.Label> */}
-            {/*   <h3>Links</h3> */}
-            {/* </DropdownMenu.Label> */}
-            {/* <DropdownMenu.Item> */}
-            {/*   Link */}
-            {/* </DropdownMenu.Item> */}
-            {/* <DropdownMenu.Item> */}
-            {/*   Link */}
-            {/* </DropdownMenu.Item> */}
-            {/* <DropdownMenu.Item> */}
-            {/*   Link */}
-            {/* </DropdownMenu.Item> */}
-            {/* <DropdownMenu.Item> */}
-            {/*   Link */}
-            {/* </DropdownMenu.Item> */}
-            {/* <DropdownMenu.Item> */}
-            {/*   Link */}
-            {/* </DropdownMenu.Item> */}
-
-            {linkItems.map((link) => (
-              <DropdownMenu.Content key={link.title}>
-                <NavLink item={link}  />
-              </DropdownMenu.Content>
-            ))}
-
-          </DropdownMenu.Content>
-        </DropdownMenu.Portal>
-      </DropdownMenu.Root>
+      <HamburgerMenuIcon 
+        onClick={onMenuClick}
+        className={`${styles.menuIcon}`}
+      />
+      {toggleMenu === 'open' && (
+        <div className={`${styles.menuContainer}`}>
+          <h2>Menu</h2>
+          {linkItems.map((link) => (
+            <NavLink item={link} key={link.title} />
+          ))}
+        </div>
+      )}
     </div>
   )
 }
